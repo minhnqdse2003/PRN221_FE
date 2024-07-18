7112491dec936dad2f0dbf7efc269572d4acb1c6
namnp.personal <namnp.personal@gmail.com>
2024-07-18 12:43:04 +0700

    update api and hook

---
+++ src/server/lessonAction.jsx
@@ -0,0 +1,45 @@
+import { fetchBase } from "./baseAction";
+
+export const getLessons = async () => {
+    const url = `${process.env.API_SECRET_URL}/api/v1/lessons`;
+  
+    const res = await fetchBase(url);
+  
+    return res;
+  };
+
+  
+export const getLesson = async (lessonId) => {
+    const url = `${process.env.API_SECRET_URL}/api/v1/lessons/${lessonId}`;
+  
+    const res = await fetchBase(url);
+  
+    return res;
+  };
+
+  export const updateLesson = async (lessonId, userData) => {
+    const url = `${process.env.API_SECRET_URL}/api/v1/lessons/${lessonId}`;
+    const options = {
+        method: 'Put',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+    };
+    const res = await fetchBase(url, options);
+    return res;
+  };
+
+  
+export const removeLesson = async (lessonId) => {
+    const url = `${process.env.API_SECRET_URL}/api/v1/lessons/${lessonId}`;
+    const options = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+    const res = await fetchBase(url, options);
+    return res;
+  };
+
--- src/server/trainingProgramAction.jsx
+++ src/server/trainingProgramAction.jsx
@@ -15,3 +15,50 @@ export const getTrainingProgram = async (id) => {
 
   return res;
 };
+
+export const removeTrainingProgram = async (id) => {
+  const url = `${process.env.API_SECRET_URL}/api/v1/trainingprograms/${id}`;
+  const options = {
+      method: 'DELETE',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+  };
+  const res = await fetchBase(url, options);
+  return res;
+};
+
+export const updateTrainingProgram = async (id, userData) => {
+  const url = `${process.env.API_SECRET_URL}/api/v1/trainingprograms/${id}`;
+  const options = {
+      method: 'Put',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+  };
+  const res = await fetchBase(url, options);
+  return res;
+};
+
+
+export const createTrainingProgram = async (id, userData) => {
+  const url = `${process.env.API_SECRET_URL}/api/v1/trainingprograms/${id}`;
+  const options = {
+      method: 'Put',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+  };
+  const res = await fetchBase(url, options);
+  return res;
+};
+
+export const getLessonsFromTrainingProgram = async (id) => {
+  const url = `${process.env.API_SECRET_URL}/api/v1/trainingprograms/${id}/lessons`;
+
+  const res = await fetchBase(url);
+
+  return res;
+};
